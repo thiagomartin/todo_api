c9530fa43073428a32f10964d4592f9dc6b70a16
thiagomartin <thiago.andriolo@live.com>
2024-05-16 17:03:42 -0300

    Implementar controlador Tasks para manipular tarefas da API

---
+++ app/controllers/tasks_controller.rb
@@ -0,0 +1,47 @@
+class TasksController < ApplicationController
+  before_action :set_task, only: [:show, :update, :destroy]
+
+  def index
+    @tasks = Task.all
+    render json: @tasks
+  end
+
+  def show
+    render json: @task
+  end
+
+  def create
+    @task = Task.new(task_params)
+
+    if @task.save
+      render json: @task, status: :created
+    else
+      render json: @task.errors, status: :unprocessable_entity
+    end
+  end
+
+  def update
+    if @task.update(task_params)
+      render json: @task
+    else
+      render json: @task.errors, status: :unprocessable_entity
+    end
+  end
+
+  def destroy
+    @task.destroy
+    head :no_content
+  end
+
+  private
+
+  def set_task
+    @task = Task.find(params[:id])
+  rescue ActiveRecord::RecordNotFound
+    render json: { error: "Tarefa não encontrada" }, status: :not_found
+  end
+
+  def task_params
+    params.require(:task).permit(:title, :description, :completed)
+  end
+end
--- db/seeds.rb
+++ db/seeds.rb
@@ -7,3 +7,9 @@
 #   ["Action", "Comedy", "Drama", "Horror"].each do |genre_name|
 #     MovieGenre.find_or_create_by!(name: genre_name)
 #   end
+# todo_api/db/seeds.rb
+
+# Criar algumas tarefas de exemplo
+Task.create(title: 'Comprar mantimentos', description: 'Ir ao supermercado e comprar itens essenciais', completed: false)
+Task.create(title: 'Estudar Ruby on Rails', description: 'Aprender a desenvolver aplicativos web com Ruby on Rails', completed: false)
+Task.create(title: 'Fazer exercícios físicos', description: 'Realizar atividades físicas diárias', completed: true)
